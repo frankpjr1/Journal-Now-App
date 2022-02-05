e2c0554455900f3529e49cf4e7328b306bbbd4e2
frankpjr1 <34253047+frankpjr1@users.noreply.github.com>
2022-02-05 03:10:16 -0500

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    colors:{
+      transparent: 'transparent',
+      current:'currentColor',      f_blue: {
+        dark:'#1f2b5e',
+        light:'#bfcbff',
+        medium:'#647ce6',
+      },
+      f_brown:{
+        medium: '#997b23',
+      },
+      f_gray:{light: '#e6e4e1',
+      },
+    },
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
