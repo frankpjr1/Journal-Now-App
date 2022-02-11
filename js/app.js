2a2211bfb8e7f1b98a6b7f2b822383baeff38313
frankpjr1 <34253047+frankpjr1@users.noreply.github.com>
2022-02-11 12:24:26 -0500

    set the form variables added an event listener and created function to check for empty inputs

--- js/app.js
+++ js/app.js
@@ -1,2 +1,35 @@
 // Logic for index.html
 
+const JnForm = document.getElementById('jn-form');
+const JnEmail = document.getElementById('email');
+const JnPassword = document.getElementById('password');
+const JnErrorMess = document.getElementById('error-message');
+
+
+//========================================================================================================//
+
+//event listener for form//
+
+JnForm.addEventListener('submit',(e)=>{
+e.preventDefault();
+if(
+checkIfEmpty(JnEmail.value,"you must enter an email")&&checkIfEmpty(JnPassword.value,"you must enter your password")
+){
+    window.location.href = '/dashboard.html';
+}
+
+
+
+});
+//checks if inputs are empty
+function checkIfEmpty(domInput,errorMessage){
+    JnErrorMess.innerHTML = "";
+    if (domInput === ""){
+        const div = document.createElement('div');
+        div.innerHTML = `<p class="w-full rounded-lg text-center font-bold">${errorMessage}</p>`;
+        JnErrorMess.appendChild(div);
+    }
+    return true;
+}
+
+//checks email and password
