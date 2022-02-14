a650dd953a105284fdc70276ac8c2b58011a3b0c
frankpjr1 <34253047+frankpjr1@users.noreply.github.com>
2022-02-14 00:41:46 -0500

    edited the event listener

--- js/app.js
+++ js/app.js
@@ -10,26 +10,32 @@ const JnErrorMess = document.getElementById('error-message');
 
 //event listener for form//
 
-JnForm.addEventListener('submit',(e)=>{
-e.preventDefault();
-if(
-checkIfEmpty(JnEmail.value,"you must enter an email")&&checkIfEmpty(JnPassword.value,"you must enter your password")
-){
-    window.location.href = '/dashboard.html';
-}
+JnForm.addEventListener('submit',(evt)=>{
+    JnErrorMess.innerHTML = "";
+    evt.preventDefault();
 
-
+    if(
+        checkNotEmpty(JnEmail.value,"you must enter a name")&&
+        checkNotEmpty(JnPassword.value,"you must enter an email")
+    ){
+        window.location.href = '/dashboard.html';
+    }
 
 });
+
+
 //checks if inputs are empty or not
-function checkIfEmpty(domInput,errorMessage){
+function checkNotEmpty(domInput,errorMessage){
     JnErrorMess.innerHTML = "";
+
+
     if (domInput === ""){
         const div = document.createElement('div');
         div.innerHTML = `<p class="w-full rounded-lg text-center font-bold">${errorMessage}</p>`;
         JnErrorMess.appendChild(div);
+        return false;
     }
-    return true;
+return true;
 }
 
 //checks email and password
